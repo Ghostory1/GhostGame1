e6ddd94c5a1c330276449b45507d61704869546b
Ghostory <ume6514@gmail.com>
2023-10-30 19:21:48 +0900

    삭제

--- Assets/Scripts/Managers/Poolable.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Poolable :MonoBehaviour
-{
-    //단지 Poolable이 가능한지 체크하는 용도의 컴포넌트
-    public bool IsUsing;
-}
