454c10e80c7faf4d3c48dd5ef7bb5892dc8acbcd
Ghostory <ume6514@gmail.com>
2024-01-19 01:12:29 +0900

    Job Queue
    Job Queue를 추가
    기존에 있던 쓰레드들이 기다리고, 계속 추가적으로 자원을 쓰던 것을 해결
    GameRoom BroadCast 에 lock 때문이였음

---
+++ Server/GameRoom.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Server.Session;
+using ServerCore;
+
+namespace Server
+{
+    class GameRoom : IJobQueue
+    {
+        List<ClientSession> _sessions = new List<ClientSession>();
+        JobQueue _jobQueue = new JobQueue();
+
+        public void Push(Action job)
+        {
+            _jobQueue.Push(job);
+        }
+
+        public void BroadCast(ClientSession session, string chat)
+        {
+            S_Chat packet = new S_Chat();
+            packet.playerId = session.SessionId;
+            packet.chat = $"{chat} I am {packet.playerId}";
+            ArraySegment<byte> segment = packet.Write();
+
+
+            // N ^ 2  -> 패킷 모아보내기
+            foreach (ClientSession s in _sessions)
+                s.Send(segment);
+            
+        }
+        public void Enter(ClientSession session)
+        {
+            
+            _sessions.Add(session);
+            session.Room = this;
+            
+            
+        }
+
+        public void Leave(ClientSession session)
+        {
+            
+            _sessions.Remove(session);
+            
+               
+        }
+
+       
+        
+    }
+}
---
+++ Server/Packet/PacketHandler.cs
@@ -0,0 +1,31 @@
+﻿using Server;
+using Server.Session;
+using ServerCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class PacketHandler
+{
+    public static void C_ChatHandler(PacketSession session, IPacket packet)
+    {
+        C_Chat chatPacket = packet as C_Chat;
+        ClientSession clientSession = session as ClientSession;
+
+        if (clientSession.Room == null)
+            return;
+
+        //바로 BroadCast를 하는게 아니고 행동을 JobQueue에 넣어준다.
+        //람다식으로 처리하고 있는데 우리가 바로 처리하는게 아니고
+        //JobQueue에 예약 한다음 처리를 해주는데
+        //참조할 Room이 중간에 사라져버리면 에러가 나니 새로운 룸으로 참조하는 식으로 크래시 방지
+        GameRoom room = clientSession.Room;
+        room.Push(() => room.BroadCast(clientSession, chatPacket.chat));
+        
+    }
+
+}
+
