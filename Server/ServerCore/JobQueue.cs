b8eaf5bbd17bfe0606d26faf76a29da9a0c7178d
Ghostory <ume6514@gmail.com>
2024-01-21 23:36:05 +0900

    패킷 모아보내기
    1. GameRoom.cs  pendingList 만들기
    
    2. BroadCast 함수에서 foreach문으로 이제 바로 뿌려주는게 아니고 pendingList에 추가만 해주기
    
    3. Server Program.cs 메인 쓰레드 에서 while 문에서 한번 뿌려보자.
    
    4.GameRoom에 Flush 함수 추가
    
    5.기존에 만들어준 Send 함수 오버로딩으로 List<ArraySegment><byte>> 버전으로 만들어서 foreach문으로 똑같이 sendQueue에 추가
    
    6.ServerCore.Session.cs 에서 OnRecv 에서 packetCount 변수 생성후 패킷 모아보내기 추적
    
    7. recvBuffer 랑 sendBuffer 다 65535 사이즈로 늘려줌

---
+++ Server/ServerCore/JobQueue.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerCore
+{
+    public interface IJobQueue
+    {
+        void Push(Action job);
+    }
+
+    public class JobQueue : IJobQueue
+    {
+        //내가 해야하는 행동들을 Queue로 가지고 있자.
+        Queue<Action> _jobQueue = new Queue<Action>();
+        object _lock = new object();
+        bool _flush = false;
+
+        public void Push(Action job)
+        {
+            bool flush = false;
+
+            lock(_lock)
+            {
+                _jobQueue.Enqueue(job);
+                if(_flush == false)
+                    flush = _flush = true;
+            }
+
+            if (flush)
+                Flush();
+        }
+
+        void Flush()
+        {
+            while(true)
+            {
+                Action action = Pop();
+                if (action == null)
+                    return;
+
+                action.Invoke();
+            }
+        }
+
+        Action Pop()
+        {
+            lock(_lock)
+            {
+                if (_jobQueue.Count == 0)
+                {
+                    _flush = false;
+                    return null;
+                }
+                
+                return _jobQueue.Dequeue();
+                    
+            }
+        }
+    }
+}
